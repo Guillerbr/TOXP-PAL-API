2e6dcef18dcb24562fc8a0a024370374cc67ff84
Guillerbr <ghminvest@gmail.com>
2020-05-01 07:32:52 -0300

    wizar-vue,quasar,vuetify libs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+//Vue Form Wizard
+//import VueFormWizard from 'vue-form-wizard';
+//import 'vue-form-wizard/dist/vue-form-wizard.min.css';
+//Vue.use(VueFormWizard)
